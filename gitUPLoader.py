ff9093bdd2a8f92d04002048ac2ee5af356c741a
yucef sourani <yucefsourani@users.noreply.github.com>
2017-06-01 17:49:35 +0300

    Fix gitStatus Function

--- gitUPLoader.py
+++ gitUPLoader.py
@@ -400,16 +400,18 @@ class git(QWidget):
         else:
             pass
 
-
     def gitStatus(self):
-
+        self.textShow.clear()
+        folderdir = self.folderDirT.text()
         try:
-            os.chdir(self.folderDirT.text())
-            if '.git' not in os.listdir() :
-
-                subprocess.check_output('git init', shell=True)
-                subprocess.check_output('git config --global user.email %s' %self.e_mailT.text())
-                subprocess.check_output('git config --global user.name %s' %self.usrNmeT.text())
+            os.chdir(folderdir)
+            if '.git' not in os.listdir(folderdir) :
+                if len(self.e_mailT.text())!=0 and len(self.usrNmeT.text())!=0:
+                    subprocess.check_output("git init", shell=True)
+                    subprocess.check_output("git config --global user.email %s" %self.e_mailT.text(),shell=True)
+                    subprocess.check_output("git config --global user.name %s" %self.usrNmeT.text(),shell=True)
+                else:
+                    raise OSError
 
 
             status = str(subprocess.check_output('git status', shell=True))
@@ -418,8 +420,8 @@ class git(QWidget):
 
             self.textShow.setText(status[2:-1])
         except:
-                self.err = error()
-                self.err.show()
+            self.err = error()
+            self.err.show()
 
 
     def addToRepo(self):
